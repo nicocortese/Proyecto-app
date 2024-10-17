7eb53a1274daeae5588370130daa8ab59a118c6a
nicocortese <nicolascortese2004@gmail.com>
2024-10-17 11:40:53 -0300

    Tu mensaje de commit

--- src/js/generalapp.js
+++ src/js/generalapp.js
@@ -1,8 +1,22 @@
-document.getElementById("btn-tirar").addEventListener('click', tirarDado);
+let dados;
 
-function tirarDado() {
+const initGame = () => {
+    dados = [0, 0, 0, 0, 0]
+}
+
+/*const drawDados = () => {
+    dados.forEach((dados, i) => document.querySelector (`.dados-container .dados.d))
+}*/
+
+
+
+/*const tirarDado() {
     for (let i = 1; i <= 5; i++) {
-        const dadoValue = Math.floor(Math.random() * 6) + 1;
-        document.getElementById(`dado${i}`).textContent = dadoValue;
+        dados[i] = Math.floor(Math.random() * 6) + 1);
+        document.getElementById(`dados${i}`).textContent = dadoValue;
     }
 }
+*/document.getElementById("btn-tirar").addEventListener('click', tirarDado);+
+
+
+document.addEventListener("DOMContentLoaded", () => { initGame() });
